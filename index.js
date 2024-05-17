c7149416fcba9c5a98c779e75e42e6fd73483628
Hansadhwaja Biswal <hansadhwajabiswal@gmail.com>
2024-05-17 14:50:02 +0530

    added

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const score = parseInt((Math.random()*100)+1);
-const submit = document.querySelector('button');
 
-submit.addEventListener('click',function (){
+const submit = document.querySelector('button');
+const firstName = document.querySelector('#inputName');
+const secondName = document.querySelector('#input');
+
+submit.addEventListener('click', function () {
+    const score = parseInt((Math.random() * 100) + 1);
     document.querySelector('.pScore').innerHTML = score;
+    firstName.value = "";
+    secondName.value = "";
+    
 })
 
